f37ea639927aa72974997c34f370375eeac98701
10zhoujiabin10 <10104518zjb>
2020-07-16 16:51:44 +0800

    Initial commit

---
+++ src/main/scala/com/atguigu/scala/HelloWorld.scala
@@ -0,0 +1,12 @@
+package com.atguigu.scala
+
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("hello world")
+    System.out.println("hello world")
+    var a=10
+    println(a)
+  }
+
+
+}
